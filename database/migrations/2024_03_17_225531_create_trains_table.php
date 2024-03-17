e9bf6039203f5640f96ed18eb3049e821684d98c
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-03-18 00:29:19 +0100

    added_migration_train

--- database/migrations/2024_03_17_225531_create_trains_table.php
+++ database/migrations/2024_03_17_225531_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('business_name', 40);
+            $table->string('starting_station', 40);
+            $table->string('last_station', 40);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code', 10);
+            $table->tinyInteger('cars_number')->unsigned();
+            $table->boolean('on_time')->default(1);
+            $table->boolean('canceled')->default(0);
             $table->timestamps();
         });
     }
