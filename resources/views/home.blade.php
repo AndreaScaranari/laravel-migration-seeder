19f5deb0d192f7699708ebf146259d3e057b11ae
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-03-17 23:40:13 +0100

    route_controller_layout_include_added

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('title', 'Home')
+
+@section('main-content')
+    <h1 class="text-center py-3">Questo è il main content</h1>
+@endsection
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,3 @@
+<header class="text-center bg-primary text-white py-3">
+    <h1>We love Trains!</h1>
+</header>
